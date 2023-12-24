1e13461f4d479ec4baaff44884c1a7574a4017c6
jewel <github@thesteve.org>
2023-12-23 21:10:28 -0700

    Connect to production server when available

--- client/lib/server.rb
+++ client/lib/server.rb
@@ -3,15 +3,21 @@
 class Server
   class << self
     def bucket_url hash
-      "#{server_url}bucket/#{hash[0...2]}/#{hash[0...4]}/#{hash}"
+      "#{server_url}/bucket/#{hash[0...2]}/#{hash[0...4]}/#{hash}"
     end
 
     def server_url
-      "http://localhost:4004/"
+      if ENV['GAME_CHANGER_SERVER']
+        ENV['GAME_CHANGER_SERVER']
+      elsif File.exist? "/etc/game-changer-server"
+        File.read("/etc/game-changer-server").chomp
+      else
+        "http://localhost:4004"
+      end
     end
 
     def get uri
-      res = HTTParty.get "#{server_url}#{uri}", format: :plain
+      res = HTTParty.get "#{server_url}/#{uri}", format: :plain
       JSON.parse res.body, symbolize_names: true
     end
 
